e58cb8cb8ae2e544f33c101b3cdad6e9e654a7a5
Jinshuo <jinshuo.dong@outlook.com>
2025-05-19 14:46:41 +0200

    key protect

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore, doc, setDoc, getDoc, updateDoc, arrayUnion, arrayRemove }
 
 // Firebase配置
 const firebaseConfig = {
-  apiKey: "AIzaSyCgAJuZ2A6LZDNMQtZUg60Ni4SgFJXixbM",
-  authDomain: "cryptotrading-b288a.firebaseapp.com",
-  projectId: "cryptotrading-b288a", 
-  storageBucket: "cryptotrading-b288a.appspot.com",
-  messagingSenderId: "374214809654",
-  appId: "1:374214809654:web:ea09275771cbf677fd6097"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // 初始化Firebase
